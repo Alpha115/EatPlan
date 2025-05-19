ef32149c60406ff96e31a857dfc8a74b52af4711
Kim_Kyoung_Whoe <kw72622@naver.com>
2025-05-19 17:29:51 +0900

    //댓글리스트

--- src/main/java/com/eat/main/MainController.java
+++ src/main/java/com/eat/main/MainController.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +28,12 @@ public class MainController {
 	@Autowired
 	MainService service;
 
+	
+	//댓글 리스트
+	
+	
+	
+	
 	// 댓글 작성
 	@PostMapping(value = "/comment_insert")
 	public Map<String, Object> insert(@RequestBody Map<String, String> params) {
