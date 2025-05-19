0f65c22f85ada458a05d184e3cb916a01cab3daa
Kim_Kyoung_Whoe <kw72622@naver.com>
2025-05-19 17:15:56 +0900

    토큰 뺌

--- src/main/java/com/eat/main/MainController.java
+++ src/main/java/com/eat/main/MainController.java
@@ -19,90 +19,55 @@ import com.eat.utils.JwtUtil;
 @CrossOrigin
 @RestController
 public class MainController {
-	
-	
 
 	Map<String, Object> resp = new HashMap<String, Object>();
 	Logger log = LoggerFactory.getLogger(getClass());
-	
-	@Autowired MainService service;
-	
-	
-	//댓글 작성
+
+	@Autowired
+	MainService service;
+
+	// 댓글 작성
 	@PostMapping(value = "/comment_insert")
-	public Map<String, Object> insert (@RequestBody Map<String, String> params
-			/*@RequestHeader Map<String, String> header*/){
-		
+	public Map<String, Object> insert(@RequestBody Map<String, String> params) {
+
 		log.info("params : " + params);
-		/* log.info("header : {} " , header); */
+
 		resp = new HashMap<String, Object>();
 		boolean login = false;
-		/* String token = header.get("authorization"); */
-		
-		/*
-		 * Map<String, Object> payload = JwtUtil.readToken(token); String loginId =
-		 * (String) payload.get("id");
-		 * 
-		 * if(loginId != null && loginId.equals(params.get("id"))) {
-		 */
-			boolean success = service.comment_insert(params);
-			resp.put("success", success);
-			login = true;
-		//}
-		
-		// resp.put("loginYN", login);
-		
+
+		boolean success = service.comment_insert(params);
+		resp.put("success", success);
+		login = true;
+
 		return resp;
 	}
-	
-	//댓글 수정
-	@PutMapping(value="/comment_update")
-	public Map<String, Object> comment_update(@RequestBody Map<String, String> params,
-			@RequestHeader Map<String, String> header){
-		
+
+	// 댓글 수정
+	@PutMapping(value = "/comment_update")
+	public Map<String, Object> comment_update(@RequestBody Map<String, String> params) {
+
 		log.info("params : " + params);
-		/* log.info("header : {} " , header); */
 		resp = new HashMap<String, Object>();
 		boolean login = false;
-		/*
-		 * String token = header.get("authorization");
-		 * 
-		 * Map<String, Object> payload = JwtUtil.readToken(token); String loginId =
-		 * (String) payload.get("id");
-		 * 
-		 * if(loginId != null && loginId.equals(params.get("user_id"))) {
-		 */
-			boolean success = service.comment_update(params);
-			resp.put("success", success);
-			login = true;
-		//}
-		/* resp.put("loginYN", login); */
-		
+		boolean success = service.comment_update(params);
+		resp.put("success", success);
+		login = true;
+
 		return resp;
 	}
-	
-	//댓글 삭제
-	@DeleteMapping(value="/comment_del")
-	public Map<String, Object> comment_del(@RequestBody Map<String, String> params, 
-			@RequestHeader Map<String, String> header){
-	
+
+	// 댓글 삭제
+	@DeleteMapping(value = "/comment_del")
+	public Map<String, Object> comment_del(@RequestBody Map<String, String> params) {
+
 		log.info("params : " + params);
-		/* log.info("header : {} " , header); */
 		resp = new HashMap<String, Object>();
 		boolean login = false;
-	/*	String token = header.get("authorization");
 		
-		Map<String, Object> payload = JwtUtil.readToken(token);
-		String loginId = (String) payload.get("id");
-		
-		if(loginId != null && loginId.equals(params.get("user_id"))) {*/
-			boolean success = service.comment_del(params);
-			resp.put("success", success);
-			login = true;
-			/*
-			 * } resp.put("loginYN", login);
-			 */
-		
+		boolean success = service.comment_del(params);
+		resp.put("success", success);
+		login = true;
+
 		return resp;
 	}
 }
