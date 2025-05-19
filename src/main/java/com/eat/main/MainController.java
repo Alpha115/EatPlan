424440b6ab05b1339af0f4563de028c43e5f1a4f
Kim_Kyoung_Whoe <kw72622@naver.com>
2025-05-19 11:23:15 +0900

    main controller  : cross origin , rest controller 추가 map, logger 추가

--- src/main/java/com/eat/main/MainController.java
+++ src/main/java/com/eat/main/MainController.java
@@ -1,5 +1,18 @@
 package com.eat.main;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
 public class MainController {
 
+	Map<String, Object> resp = new HashMap<String, Object>();
+	Logger log = LoggerFactory.getLogger(getClass());
+	
 }
