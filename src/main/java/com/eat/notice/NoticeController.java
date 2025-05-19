c4f268fa51a9b9be5c7e696fb582db9b9891d364
Alpha <118242780+Alpha115@users.noreply.github.com>
2025-05-19 14:34:02 +0900

    공지사항 기능 완성

--- src/main/java/com/eat/notice/NoticeController.java
+++ src/main/java/com/eat/notice/NoticeController.java
@@ -1,12 +1,16 @@
 package com.eat.notice;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +23,47 @@ public class NoticeController {
 	Map<String, Object> resp = null;
 
 	// 공지사항 리스트를 불러옵니다.
-	@GetMapping("/notice")
-	public Map<String, Object> list(@RequestHeader Map<String, String> header) {
+	@GetMapping("/notice/{page}")
+	public Map<String, Object> list(@RequestHeader Map<String, String> header, @PathVariable String page) {
 		resp = new HashMap<String, Object>();
-		ArrayList<NoticeDTO> noticeList = service.list();
+		Map<String, Object> noticeList = service.list(page);
 		resp.put("noticeList", noticeList);
+		return resp;
+	}
+	
+	// 공지사항 상세보기 기능입니다.
+	@GetMapping("/notice_detail/{notice_idx}")
+	public Map<String, Object> detail(@PathVariable String notice_idx){
+		resp=new HashMap<String, Object>();
+		NoticeDTO dto=service.detail(notice_idx);
+		resp.put("detail", dto);
+		return resp;
+	}
+
+	// 공지사항을 작성합니다.
+	@PostMapping("/notice_write")
+	public Map<String, Object> write(@RequestBody Map<String, Object> params) {
+		resp = new HashMap<String, Object>();
+		boolean success = service.write(params);
+		resp.put("success", success);
+		return resp;
+	}
+	
+	// 공지사항을 수정합니다.
+	@PutMapping("/notice_update")
+	public Map<String, Object> update(@RequestBody NoticeDTO params){
+		resp=new HashMap<String, Object>();
+		boolean success = service.update(params);
+		resp.put("success", success);
+		return resp;
+	}
+	
+	//공지사항을 삭제합니다.
+	@DeleteMapping("/notice_del/{notice_idx}")
+	public Map<String, Object> delete(@PathVariable String notice_idx){
+		resp=new HashMap<String, Object>();
+		boolean success=service.delete(notice_idx);
+		resp.put("success", success);
 		return resp;
 	}
 
--- src/main/java/com/eat/notice/NoticeService.java
+++ src/main/java/com/eat/notice/NoticeService.java
@@ -1,6 +1,8 @@
 package com.eat.notice;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,12 +12,48 @@ import org.springframework.stereotype.Service;
 @Service
 public class NoticeService {
 
-	@Autowired NoticeDAO dao;
+	@Autowired
+	NoticeDAO dao;
 	Logger log = LoggerFactory.getLogger(getClass());
+	private int limit = 8, page = 0; // 1페이지당 뜨는 공지사항 게시물의 갯수입니다.
 
-	public ArrayList<NoticeDTO> list() {
+	public Map<String, Object> list(String page) {
+		Map<String, Object> result = new HashMap<String, Object>();
 		log.info("Notice/Service에서 list를 호출하였습니다.");
-		return dao.list();
+		this.page=Integer.parseInt(page);
+		result.put("page", this.page);
+		int offset = (this.page - 1) * limit;
+		result.put("list", dao.list(offset, limit));
+		result.put("pages", dao.pages(limit));
+		
+		return result;
+	}
+
+	public boolean write(Map<String, Object> params) {
+		log.info("Notice/Service에서 write를 호출하였습니다.");
+		int row = dao.write(params);
+		return row > 0 ? true : false;
+	}
+
+	public NoticeDTO detail(String notice_idx) {
+		upHit(notice_idx);
+		return dao.detail(notice_idx);
+	}
+
+	// 해당 게시글의 조회수를 1 올리는 함수입니다.
+	public void upHit(String idx) {
+		int row = dao.upHit(idx);
+		log.info("Notice/Service 에서 upHit을 호출하였습니다. / {}", row);
+	}
+
+	public boolean update(NoticeDTO params) {
+		int row = dao.update(params);
+		return row > 0 ? true : false;
+	}
+
+	public boolean delete(String notice_idx) {
+		int row = dao.delete(notice_idx);
+		return row > 0 ? true : false;
 	}
 
 }
