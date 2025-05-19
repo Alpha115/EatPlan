0385594c9154c5fc0430454f07669d9fe7221ffd
Kim_Kyoung_Whoe <kw72622@naver.com>
2025-05-19 11:56:15 +0900

    컨트롤러 , 서비스 , 다오 오토와이어드랑 로거랑 서비스 어노테이션 함

--- src/main/java/com/eat/main/MainController.java
+++ src/main/java/com/eat/main/MainController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,7 @@ public class MainController {
 
 	Map<String, Object> resp = new HashMap<String, Object>();
 	Logger log = LoggerFactory.getLogger(getClass());
+	
+	@Autowired MainService service;
 	
 }
--- src/main/java/com/eat/main/MainDAO.java
+++ src/main/java/com/eat/main/MainDAO.java
@@ -1,5 +1,8 @@
 package com.eat.main;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface MainDAO {
 
 }
