ddc1fc921f27e7e7ba09b581f6463907665366ff
kw72622 <kw72622@naver.com>
2025-05-20 22:52:18 +0900

    Update MsgDAO.java
    보낸 쪽지 보기

--- src/main/java/com/eat/msg/MsgDAO.java
+++ src/main/java/com/eat/msg/MsgDAO.java
@@ -7,9 +7,13 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MsgDAO {
 
-  //쪽지 보내기
+	//쪽지 보내기
 	int write_msg(Map<String, String> params);
 
-//받은 쪽지 보기
+	//받은 쪽지 보기
 	List<MsgDTO> recip_msg(String user_id, int offset, int msg_count);
+	
+	//보낸 쪽지 보기
+	List<MsgDTO> send_msg(String user_id, int offset, int msg_count);
+
 }
