e0bb67324b943c6b2e9c838f77aeca0cb255910e
kw72622 <kw72622@naver.com>
2025-05-20 22:17:23 +0900

    Update MsgService.java
    쪽지보내기 서비스

--- src/main/java/com/eat/msg/MsgService.java
+++ src/main/java/com/eat/msg/MsgService.java
@@ -1,5 +1,24 @@
 package com.eat.msg;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MsgService {
+	
+	@Autowired
+	MsgDAO dao;
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	
+	//쪽지 보내기
+	public boolean write_msg(Map<String, String> params) {
+		int row = dao.write_msg(params);
+		return row > 0;
+	}
 
 }
