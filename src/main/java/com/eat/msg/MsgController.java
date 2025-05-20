403cfd9e24d65f95f8ddb451bcb3e9aaf6646f6d
kw72622 <kw72622@naver.com>
2025-05-20 22:16:45 +0900

    Update MsgController.java
    쪽지 작성

--- src/main/java/com/eat/msg/MsgController.java
+++ src/main/java/com/eat/msg/MsgController.java
@@ -1,5 +1,39 @@
 package com.eat.msg;
 
-public class MsgController {
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class MsgController {
+	
+	@Autowired
+	MsgService service;
+	Map<String, Object> resp = new HashMap<String, Object>();
+	Logger log = LoggerFactory.getLogger(getClass());
+
+	//쪽지 작성
+	@PostMapping(value="/{user_id}/write_msg")
+	public Map<String, Object>write_msg(@PathVariable String user_id,
+			@RequestBody Map<String, String>params){
+		
+		log.info("user_id: " + user_id);
+        log.info("params : " + params);
+        
+        resp = new HashMap<String, Object>();
+        params.put("user_id", user_id);
+        boolean success = service.write_msg(params);
+        resp.put("success", success);
+        
+		return resp;
+	}
 }
