dcf8d71809fc11498781278e077df96d9b5badf3
Kim_Kyoung_Whoe <kw72622@naver.com>
2025-05-20 17:45:18 +0900

    프로필 사진 사진 저장  / 변경  기능

--- src/main/java/com/eat/mypage/MypageService.java
+++ src/main/java/com/eat/mypage/MypageService.java
@@ -3,8 +3,10 @@ package com.eat.mypage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
 import java.net.URLEncoder;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -19,8 +21,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
-import ch.qos.logback.core.subst.Token.Type;
 
 @Service
 public class MypageService {
@@ -42,7 +42,7 @@ public class MypageService {
 			for (MultipartFile file : files) {
 				String fileSaved = fileSave(file);
 			
-				int newImgIdx = dao.saveProfileImg(fileSaved);
+				int newImgIdx = dao.saveProfileImg(fileSaved); // 사진 DB에 저장
 			dto.setImg_idx(newImgIdx);
 			}
 			
@@ -52,13 +52,38 @@ public class MypageService {
 	
 	//프로필 사진 저장
 	private String fileSave(MultipartFile file) {
+		boolean success = false;
 		
-		return null;
+		// 확장자 추출해서
+		String ori_fileName = file.getOriginalFilename();
+		String ext = "";
+		if(ori_fileName != null && ori_fileName.contains(".")) {
+			ext = ori_fileName.substring(ori_fileName.lastIndexOf("."));
+		}
+		
+		// 새 파일명 만들고
+		String new_filename = UUID.randomUUID().toString() + ext;
+		
+		// 저장 경로 정하고
+		String imgDir =  "c:/upload";
+		File profPath = new File(imgDir);
+		
+		//저장 경로 없으면 만들라고 시키고
+		if(!profPath.exists()) {
+			profPath.mkdirs();
+		}
+		
+		//파일 저장 되서
+		try {
+			file.transferTo(new File(imgDir + new_filename));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		//반환 되게 하기
+		return new_filename;
 	}
-
-	//프로필 사진 저장
-	
-
 	
 
 	// 선호 태그 수정
