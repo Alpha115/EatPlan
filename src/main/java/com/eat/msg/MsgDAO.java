56a79c6788bf634e4e1732fc3e02d93362688c2a
kw72622 <kw72622@naver.com>
2025-05-21 03:07:11 +0900

    Update MsgDAO.java
    쪽지수정

--- src/main/java/com/eat/msg/MsgDAO.java
+++ src/main/java/com/eat/msg/MsgDAO.java
@@ -1,27 +1,29 @@
 package com.eat.msg;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MsgDAO {
-
+	
 	//쪽지 보내기
 	int write_msg(Map<String, String> params);
-
+	
 	//받은 쪽지 보기
 	List<MsgDTO> recip_msg(String user_id, int offset, int msg_count);
 	
 	//보낸 쪽지 보기
 	List<MsgDTO> send_msg(String user_id, int offset, int msg_count);
-
+	
 	//받은 쪽지 삭제
 	int recip_del(String user_id, int msg_idx);
 	
 	//보낸 쪽지 삭제
 	int send_del(String user_id, int msg_idx);
-
+	
 	//쪽지 상세보기
 	MsgDTO msg_detail(String user_id, int msg_idx);
+
 }
