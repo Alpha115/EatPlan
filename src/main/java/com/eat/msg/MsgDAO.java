05b517f2fcc75db7e9cd97b07c13ed99e2349196
kw72622 <kw72622@naver.com>
2025-05-20 22:18:01 +0900

    Update MsgDAO.java
    쪽지보내기

--- src/main/java/com/eat/msg/MsgDAO.java
+++ src/main/java/com/eat/msg/MsgDAO.java
@@ -1,5 +1,13 @@
 package com.eat.msg;
 
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface MsgDAO {
+
+  //쪽지 보내기
+	int write_msg(Map<String, String> params);
 
 }
