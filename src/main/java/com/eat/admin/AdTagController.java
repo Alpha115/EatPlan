410b28a312abd3da82eeb058b459bf2cca116cae
Alpha <118242780+Alpha115@users.noreply.github.com>
2025-05-23 15:35:15 +0900

    식당에 태그를 추가하는 기능

--- src/main/java/com/eat/admin/AdTagController.java
+++ src/main/java/com/eat/admin/AdTagController.java
@@ -1,6 +1,5 @@
 package com.eat.admin;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,20 +34,15 @@ public class AdTagController {
 		return resp;
 	}
 	
-	// (관리자 페이지)식당에 태그를 추가하는 함수입니다. 지역 태그는 포함되지 않습니다. 
-	// 식당에 포함될 수 있는 식당 태그에 제한은 없습니다. 
-	// 특정 식당을 식별하는 resta_idx는 요청에 따라 고정적으로 들어옵니다.
-	// 그런데 tag_idx를 못받네? 걍 Map<tag_name, resta_idx> 로 받습니다
-	@PostMapping("/adtag_restaTag")
-	public Map<String, Object> restaTag(@RequestBody ArrayList<Map<String, Integer>> tags){
+	// (관리자 페이지) 식당에 식당 태그/지역 태그를 추가합니다.
+	// 지역 태그는 FK로, insert가 아닌 update가 이루어져야 합니다.
+	@PostMapping("/adtag_addTagResta")
+	public Map<String, Object> addTags(@RequestBody Tags tags){
 		resp=new HashMap<String, Object>();
-		boolean success=service.restaTag(tags);
+		boolean success=service.addTags(tags);
 		resp.put("success", success);
 		return resp;
 	}
-	// 위에꺼 만들어졌니...? 식당에 지역태그 추가해야하지 않겠니?
-	// 코스 태그 추가 해야되지 않겠니...?
-	
 	
 	
 	
@@ -110,5 +104,36 @@ public class AdTagController {
 		
 		return resp;
 	}
+	
+}
+
+
+
+class Tags{
+	
+	private int[] tag_idx;	// 태그 n개
+	private int resta_idx;	// 식당 1개
+	private int area_tag_idx;	//지역태그 1개
+	
+	public int[] getTag_idx() {
+		return tag_idx;
+	}
+	public void setTag_idx(int[] tag_idx) {
+		this.tag_idx = tag_idx;
+	}
+	public int getResta_idx() {
+		return resta_idx;
+	}
+	public void setResta_idx(int resta_idx) {
+		this.resta_idx = resta_idx;
+	}
+	public int getArea_tag_idx() {
+		return area_tag_idx;
+	}
+	public void setArea_tag_idx(int area_tag_idx) {
+		this.area_tag_idx = area_tag_idx;
+	}
+	
+	
 	
 }
