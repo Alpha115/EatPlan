ac3fe2254dcdad7e3c254e204e108dabec5ea33f
Alpha <118242780+Alpha115@users.noreply.github.com>
2025-05-28 16:06:05 +0900

    Update MainController.java

--- src/main/java/com/eat/main/MainController.java
+++ src/main/java/com/eat/main/MainController.java
@@ -11,13 +11,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eat.dto.CourseDTO;
 import com.eat.dto.RegistRequestDTO;
-import com.eat.utils.JwtUtil;
 
 @CrossOrigin
 @RestController
